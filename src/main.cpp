de9ccaf376ec16b73ca5883a221ae657fce5dfee
Reghardt <reghardt1000@gmail.com>
2023-06-05 12:33:53 +0200

    switch between test and prod mode

--- src/main.cpp
+++ src/main.cpp
@@ -9,48 +9,68 @@ int myFunction(int, int);
 
 NewPing ultra = NewPing(2, 3, MAX_DISTANCE);
 
-int interval=1000;
+bool testMode = false;
+
+unsigned int interval=1000;
 unsigned long previousMillis=0;
+
+int reading = 100;
+bool isFilling = true;
 
 void setup() {
   pinMode(RS_MODE_PIN, OUTPUT);
   digitalWrite(RS_MODE_PIN, HIGH); // if low, is receiver
 
   Serial.begin(9600);
+
+  if(!testMode)
+  {
+    interval = 3000;
+  }
 }
 
 
-int reading = 100;
-bool isFilling = true;
+
 
 //   ###S1:294!$$$
 void loop() {
-  //auto reading = ultra.ping_cm();
+  
 
   unsigned long currentMillis = millis();
 
 
 
+
+
      
   if((unsigned long)(currentMillis - previousMillis) >= interval) {
-    Serial.println("###SR:" + String(reading) + "!$$$");
-
-    if(isFilling)
+    if(testMode)
     {
-      reading++;
-      if(reading >= 300)
+      Serial.println("###SR:" + String(reading) + "!$$$");
+
+      if(isFilling)
       {
-        isFilling = false;
+        reading++;
+        if(reading >= 300)
+        {
+          isFilling = false;
+        }
+      }
+      else
+      {
+        reading--;
+        if(reading <= 100)
+        {
+          isFilling = true;
+        }
       }
     }
     else
     {
-      reading--;
-      if(reading <= 100)
-      {
-        isFilling = true;
-      }
+      auto ultraReading = ultra.ping_cm();
+      Serial.println("###SR:" + String(ultraReading) + "!$$$");
     }
+
     
 
     previousMillis = currentMillis;
